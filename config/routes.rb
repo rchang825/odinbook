5c3640f149ef1c6f87e151fb7809340fde7aaa7c
Ruby Chang <rchang825@hotmail.com>
2024-09-04 16:36:23 -0700

    User's profile shows information and user's posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     }
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users
   resources :posts do
     resources :likes
     resources :comments
