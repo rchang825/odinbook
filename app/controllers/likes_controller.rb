3bee3904186f7fa5bbac1f7a353c08070a91acd3
Ruby Chang <rchang825@hotmail.com>
2024-09-09 16:04:11 -0700

    Update like function

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,18 @@ class LikesController < ApplicationController
     if !liked?
       @like = @likeable.likes.new(user_id: current_user.id)
       @like.user = current_user
-      @like.save
+      likeable_type = @like.likeable_type
+      if @like.save
+        if likeable_type == "Image"
+          @image = Image.find(@likeable.id)
+          render partial: "images/image", locals: {image: @image}
+        else
+          @post = Post.find(@likeable.id)
+          render partial: "posts/post", locals: {post: @post}
+        end
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
   end
 
